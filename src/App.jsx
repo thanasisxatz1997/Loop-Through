11158fd96a7b418d2b59cd989798096d6e55d6a4
thanasisxatz1997 <thanasisxatz1997@hotmail.com>
2024-03-23 16:20:27 +0200

    GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Login from "../src/pages/Login";
 import Courses from "../src/pages/Courses";
 import Quizes from "../src/pages/Quizes";
 import AppLayout from "./ui/AppLayout";
+import GlobalStyles from "./styles/GlobalStyles";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="home" />}></Route>
-          <Route path="home" element={<Home />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="courses" element={<Courses />}></Route>
-          <Route path="quizes" element={<Quizes />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="home" />}></Route>
+            <Route path="home" element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="courses" element={<Courses />}></Route>
+            <Route path="quizes" element={<Quizes />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
