bd9de35d319b69004ea92152265526fa448ad607
thanasisxatz1997 <thanasisxatz1997@hotmail.com>
2024-04-25 16:16:14 +0300

    fakeAPI

---
+++ src/fakeAPI/fakeLessonContent.js
@@ -0,0 +1,21 @@
+export const lessonContentElements = [
+  { id: 1, type: "t", content: "This is a title", size: "1" },
+  { id: 2, type: "p", content: "This is a paragraph" },
+  { id: 3, type: "p", content: "This is another paragraph" },
+  { id: 4, type: "t", content: "This is another title" },
+  {
+    id: 5,
+    type: "i",
+    content: "",
+    sizeX: "auto",
+    sizeY: "auto",
+  },
+  { id: 6, type: "t", content: "God damn it im good at this" },
+];
+
+export const lessonQuizes = [
+  { title: "quiz1" },
+  { title: "quiz2" },
+  { title: "quiz3" },
+  { title: "quiz4" },
+];
--- src/ui/LessonContent.jsx
+++ src/ui/LessonContent.jsx
@@ -3,33 +3,19 @@ import LessonTitle from "./LessonTitle";
 import Row from "../styles/Row";
 import LessonQuizesContainer from "../ui/LessonQuizesContainer";
 import LessonElement from "../services/LessonElement";
+import {
+  lessonContentElements,
+  lessonQuizes,
+} from "../fakeAPI/fakeLessonContent";
 function LessonContent({ id }) {
-  const lessonContentElements = [
-    { id: 1, type: "t", content: "This is a title", size: "1" },
-    { id: 2, type: "p", content: "This is a paragraph" },
-    { id: 3, type: "p", content: "This is another paragraph" },
-    { id: 4, type: "t", content: "This is another title" },
-    {
-      id: 5,
-      type: "i",
-      content: "",
-      sizeX: "auto",
-      sizeY: "auto",
-    },
-    { id: 6, type: "t", content: "God damn it im good at this" },
-  ];
-  const tempQuizes = [
-    { title: "quiz1" },
-    { title: "quiz2" },
-    { title: "quiz3" },
-    { title: "quiz4" },
-  ];
+  const tempLessonContentElements = lessonContentElements;
+  const tempQuizes = lessonQuizes;
 
   return (
     <Row type="vertical">
       <LessonTitle title={`This is lesson number ${id}`}></LessonTitle>
       <StyledLessonContent>
-        {lessonContentElements.map((element) => (
+        {tempLessonContentElements.map((element) => (
           <LessonElement key={element.id} element={element}></LessonElement>
         ))}
       </StyledLessonContent>
