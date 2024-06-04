281540e469c456f38fcffaa2a77894b36e9af490
achatziath <thanasisxatz1997@hotmail.com>
2024-06-04 16:17:56 +0300

    test

--- src/features/courses/CreateLessonForm.jsx
+++ src/features/courses/CreateLessonForm.jsx
@@ -22,6 +22,7 @@ function CreateLessonForm() {
         <StyledButton>Create</StyledButton>
         <StyledButton variation="danger">Cancel</StyledButton>
       </Row>
+      <Row></Row>
     </div>
   );
 }
