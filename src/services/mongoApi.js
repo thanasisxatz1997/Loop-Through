f7e1ea85bdbb7947d1c0cf783b6ca187afb11e33
achatziath <achatziath@groupnet.gr>
2025-06-27 16:13:30 +0300

    Update mongoApi.js

--- src/services/mongoApi.js
+++ src/services/mongoApi.js
@@ -1,3 +1,3 @@
-export const apiUrl = "http://localhost:8080/api";
+//export const apiUrl = "http://localhost:8080/api";
 //export const apiUrl ="https://looopthroughbackserv-production.up.railway.app/api";
-// export const apiUrl = "https://api.starkgript.eu/api";
+export const apiUrl = "https://api.starkgript.eu/api";
