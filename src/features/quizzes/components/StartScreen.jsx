09b70e01ea0870f2a4affaa6fe66df232ec546b7
achatziath <achatziath@groupnet.gr>
2025-08-26 12:57:14 +0300

    Update StartScreen.jsx

--- src/features/quizzes/components/StartScreen.jsx
+++ src/features/quizzes/components/StartScreen.jsx
@@ -5,7 +5,7 @@ function StartScreen() {
   return (
     <div className="start">
       <h2>{quizName}</h2>
-      <h3>{numQuestions} question to test your React mastery</h3>
+      <h3>{numQuestions} questions</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
